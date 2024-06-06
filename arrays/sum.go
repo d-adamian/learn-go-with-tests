94181e4e6eccfb74eb873af130db985a7eeec7bf
d-adamian <dmitry.adamian@gmail.com>
2024-06-06 16:56:29 +0300

    Arrays & slices exercise

---
+++ arrays/sum.go
@@ -0,0 +1,34 @@
+package arrays
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersToSum {
+		result = append(result, Sum(numbers))
+	}
+
+	return result
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var result []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
+			tail := numbers[1:]
+			result = append(result, Sum(tail))
+		}
+	}
+
+	return result
+}
