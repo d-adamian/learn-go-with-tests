d11b76bec32b151f883a15a5f0453cfb0c33b6f4
d-adamian <dmitry.adamian@gmail.com>
2024-06-13 14:23:49 +0300

    Maps exercise

---
+++ maps_tdd/dictionary.go
@@ -0,0 +1,53 @@
+package maps_tdd
+
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+var (
+	ErrNotFound         = DictionaryErr("could not find word")
+	ErrWordExists       = DictionaryErr("word already exists")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+		return nil
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+		return nil
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
---
+++ maps_tdd/go.mod
@@ -0,0 +1,3 @@
+module maps_tdd
+
+go 1.22
