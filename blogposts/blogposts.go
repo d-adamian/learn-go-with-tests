e7cb18fe4a03d08f1dcfbe412e5105a76d55f43a
d-adamian <dmitry.adamian@gmail.com>
2024-12-24 18:09:10 +0300

    Blogposts exercise

---
+++ blogposts/blogposts.go
@@ -0,0 +1,83 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"strings"
+)
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagSeparator         = "Tags: "
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+func NewPostsFromFs(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		line := scanner.Text()
+		return strings.TrimPrefix(line, tagName)
+	}
+
+	title := readMetaLine(titleSeparator)
+	description := readMetaLine(descriptionSeparator)
+	tagsLine := readMetaLine(tagSeparator)
+	tags := strings.Split(tagsLine, ", ")
+	body := readBody(scanner)
+
+	post := Post{
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        body,
+	}
+	return post, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan()
+
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,58 @@
+package blogposts_test
+
+import (
+	"blogposts"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+K
+G
+B`
+	)
+
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFs(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("Got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+// Continue at https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reading-files#write-the-test-first-2
